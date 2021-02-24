517ad0cb2277026c30e9548e636bdb71615111c2
victorleite06 <66479019+victorleite06@users.noreply.github.com>
2021-02-24 15:55:12 -0300

    Update

--- Tp1/AlgebraBooleana.java
+++ Tp1/AlgebraBooleana.java
@@ -1,10 +1,22 @@
 class AlgebraBooleana{
-	public static void Algebra(int var,int x[],String in){
-		int cont = 0;
+	public static String replace(String in,String x,char change){
+		boolean repl;
+		String aux = "";
 		for(int i = 0;i < in.length();i++){
-			if(in.charAt(i) == '(')
-				cont++;
+			repl = true;
+			if(in.charAt(i) == x.charAt(0)){
+				for(int j = 0;j < x.length();j++){
+					if(!(in.charAt(j) == x.charAt(j))){
+						repl = false;
+					}
+				}
+				if(repl)
+					aux += change;
+				i += x.length();
+			}
+			aux += in.charAt(i);
 		}
+		return aux;
 	}
 
 	public static void main(String[] args){
@@ -14,7 +26,37 @@ class AlgebraBooleana{
 			for(int i = 0;i < var;i++)
 				x[i] = MyIO.readInt();
 			String in = MyIO.readLine();
-			Algebra(var,x,in);
+			in = replace(in,"A",(char)(x[0]));
+			in = replace(in,"B",(char)(x[1]));
+			if(var == 3)
+				in = replace(in,"C",(char)(x[3]));
+			in = replace(in,"not(0)",'1');
+			in = replace(in,"not(1)",'0');
+			in = replace(in,"and(0 , 0)",'0');
+			in = replace(in,"and(1 , 0)",'0');
+			in = replace(in,"and(1 , 1)",'1');
+			in = replace(in,"and(0 , 1)",'0');
+			in = replace(in,"or(1 , 0)",'1');
+			in = replace(in,"or(1 , 1)",'1');
+			in = replace(in,"or(0 , 0)",'0');
+			in = replace(in,"or(0 , 1)",'1');
+			in = replace(in,"and(1 , 1, 1)",'1');
+			in = replace(in,"and(1 , 0, 1)",'0');
+			in = replace(in,"and(1 , 1, 0)",'0');
+			in = replace(in,"and(1 , 0, 0)",'0');
+			in = replace(in,"and(0 , 1, 1)",'0');
+			in = replace(in,"and(0 , 1, 0)",'0');
+			in = replace(in,"and(0 , 0, 1)",'0');
+			in = replace(in,"and(0 , 0, 0)",'0');
+			in = replace(in,"or(1 , 1, 1)",'1');
+			in = replace(in,"or(1 , 0, 1)",'1');
+			in = replace(in,"or(1 , 1, 0)",'1');
+			in = replace(in,"or(1 , 0, 0)",'1');
+			in = replace(in,"or(0 , 1, 1)",'1');
+			in = replace(in,"or(0 , 1, 0)",'1');
+			in = replace(in,"or(0 , 0, 1)",'1');
+			in = replace(in,"or(0 , 0, 0)",'0');
+			MyIO.println(in);
 			var = MyIO.readInt();
 		}
 	}
