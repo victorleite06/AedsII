6ac13a77f629751af881bd10fbf185faf3a32cfe
victorleite06 <66479019+victorleite06@users.noreply.github.com>
2021-03-02 09:50:00 -0300

    Update

--- Tp2/Classe.java
+++ Tp2/Classe.java
@@ -1,4 +1,6 @@
 class Musica{
+//Declaração de variáveis 
+//-------------------------------------------------------------
     private int id;
     private String nome;
     private String key;
@@ -16,7 +18,9 @@ class Musica{
     private double loudness;
     private double speechiness;
     private int year;
-    
+//-------------------------------------------------------------
+//Construtor da Classe
+//-------------------------------------------------------------
     public Musica(int id,String nome,String key,double acousticness,double danceability,double energy, int duration_ms,double instrumentalness,double valence,int popularity,float tempo,double liveness,double loudness,double speechiness,int year){
         this.id = id;
         this.nome = nome;
@@ -34,7 +38,100 @@ class Musica{
         this.speechiness = speechiness;
         this.year = year;
     }
-
+//-------------------------------------------------------------
+//Sets e Gets
+//-------------------------------------------------------------
+    public void setId(int id){
+        this.id = id;
+    }
+    public int getId(){
+        return this.id;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getNome(){
+        return this.nome;
+    }
+    public void setKey(String key){
+        this.key = key;
+    }
+    public String getKey(){
+        return this.key;
+    }
+    public void setAcousticness(double acousticness){
+        this.acousticness = acousticness;
+    }
+    public double getAcousticness(){
+        return this.acousticness;
+    }
+    public void setDanceability(double danceability){
+        this.danceability = danceability;
+    }
+    public double getDanceability(){
+        return this.danceability;
+    }
+    public void setEnergy(double energy){
+        this.energy = energy;
+    }
+    public double getEnergy(){
+        return this.energy;
+    }
+    public void setDuration_ms(int duration_ms){
+        this.duration_ms = duration_ms;
+    }
+    public int getDuration_ms(){
+        return this.duration_ms;
+    }
+    public void setInstrumentalness(double instrumentalness){
+        this.instrumentalness = instrumentalness;
+    }
+    public double getInstrumentalness(){
+        return this.instrumentalness;
+    }
+    public void setValence(double valence){
+        this.valence = valence;
+    }
+    public double getValence(){
+        return this.valence;
+    }
+    public void setPopularity(int popularity){
+        this.popularity = popularity;
+    }
+    public int getPopularity(){
+        return this.popularity;
+    }
+    public void setTempo(float tempo){
+        this.tempo = tempo;
+    }
+    public float getTempo(){
+        return this.tempo;
+    }
+    public void setLiveness(double liveness){
+        this.liveness = liveness;
+    }
+    public double getLiveness(){
+        return this.liveness;
+    }
+    public void setLoudness(double loudness){
+        this.loudness = loudness;
+    }
+    public double getLoudness(){
+        return this.loudness;
+    }
+    public void setSpeechiness(double speechiness){
+        this.speechiness = speechiness;
+    }
+    public double getSpeechiness(){
+        return this.speechiness;
+    }
+    public void setYear(int year){
+        this.year = year;
+    }
+    public int getYear(){
+        return this.year;
+    }
+//-------------------------------------------------------------
 }
 
 class Classe{
