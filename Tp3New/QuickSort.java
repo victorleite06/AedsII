e84d8ed8c06586312e64e4272ac82bfa111e12b5
victorleite06 <victor.leite2001@gmail.com>
2021-10-25 15:30:54 -0300

    Update

---
+++ Tp3New/QuickSort.java
@@ -0,0 +1,294 @@
+import java.io.*;
+import java.io.FileReader;
+
+class Serie{
+    private String nome;
+    private String formato;
+    private String duracao;
+    private String paisOrigem;
+    private String idiomaOrigem;
+    private String emissoraTV;
+    private String trasmissaoOriginal;
+    private int numTemp;
+    private int numEp;
+    //------------------------------------------------------------------------------
+    //Construtores
+    public Serie(){
+        this.nome = "";
+        this.formato = "";
+        this.duracao = "";
+        this.paisOrigem = "";
+        this.idiomaOrigem = "";
+        this.emissoraTV = "";
+        this.trasmissaoOriginal = "";
+        this.numTemp = 0;
+        this.numEp = 0;
+    }
+    public Serie(String nome, String formato, String duracao, String paisOrigem, String idiomaOrigem, String emissoraTV, String transmissaoOriginal, int numTemp, int numEp){
+        this.nome = nome;
+        this.formato = formato;
+        this.duracao = duracao;
+        this.paisOrigem = paisOrigem;
+        this.idiomaOrigem = idiomaOrigem;
+        this.emissoraTV = emissoraTV;
+        this.trasmissaoOriginal = transmissaoOriginal;
+        this.numTemp = numTemp;
+        this.numEp = numEp;
+    }
+    //------------------------------------------------------------------------------
+    //Sets/Gets
+    public void setNome(String nome){ this.nome = nome; }
+    public String getNome(){ return this.nome; }
+
+    public void setFormato(String formato){ this.formato = formato; }
+    public String getFormato(){ return this.formato; }
+    
+    public void setDuracao(String duracao){ this.duracao = duracao; }
+    public String getDuracao(){ return this.duracao; }
+    
+    public void setPaisOrigem(String paisOrigem){ this.paisOrigem = paisOrigem; }
+    public String getPaisOrigem(){ return this.paisOrigem; }
+    
+    public void setIdiomaOrigem(String idiomaOrigem){ this.idiomaOrigem = idiomaOrigem; }
+    public String getIdiomaOrigem(){ return this.idiomaOrigem; }
+    
+    public void setEmissoraTV(String emissoraTV){ this.emissoraTV = emissoraTV; }
+    public String getEmissoraTV(){ return this.emissoraTV; }
+    
+    public void setTransmissaoOrigem(String transmissaoOriginal){ this.trasmissaoOriginal = transmissaoOriginal; }
+    public String getTransmissaoOriginal(){ return this.trasmissaoOriginal; }
+    
+    public void setNumTemp(int numTemp){ this.numTemp = numTemp; }
+    public int getNumTemp(){ return this.numTemp; }
+    
+    public void setNumEp(int numEp){ this.numEp = numEp; }
+    public int getNumEp(){ return this.numEp; }
+    //------------------------------------------------------------------------------
+    //Clone/Ler
+    public Serie clone(){
+        Serie novo = new Serie();
+        novo.nome = this.nome;
+        novo.formato = this.formato;
+        novo.duracao = this.duracao;
+        novo.paisOrigem = this.paisOrigem;
+        novo.idiomaOrigem = this.idiomaOrigem;
+        novo.emissoraTV = this.emissoraTV;
+        novo.trasmissaoOriginal = this.trasmissaoOriginal;
+        novo.numTemp = this.numTemp;
+        novo.numEp = this.numEp;
+        return novo;
+    }
+    
+    public void ler(String Arqu){
+        //String nomeArq = "/tmp/series/" + Arqu; // Verde
+        String nomeArq = Arqu; // Teste
+
+        try{
+            FileReader arq = new FileReader(nomeArq);
+            BufferedReader ler = new BufferedReader(arq);
+
+            this.nome = procurarNome(Arqu);
+
+            while(!(ler.readLine().contains("Formato")));
+            this.formato = removerTags(ler.readLine());
+
+            while(!(ler.readLine().contains("Duração")));
+            this.duracao = removerTags(ler.readLine());
+
+            while(!(ler.readLine().contains("País de origem")));
+            this.paisOrigem = removerTags(ler.readLine());
+
+            while(!(ler.readLine().contains("Idioma original")));
+            this.idiomaOrigem = removerTags(ler.readLine());
+
+            while(!(ler.readLine().contains("Emissora de televisão")));
+            this.emissoraTV = removerTags(ler.readLine());
+
+            while(!(ler.readLine().contains("Transmissão original")));
+            this.trasmissaoOriginal = removerTags(ler.readLine());
+
+            while(!(ler.readLine().contains("N.º de temporadas")));
+            this.numTemp = retornarInt(removerTags(ler.readLine()));
+
+            while(!(ler.readLine().contains("N.º de episódios")));
+            this.numEp = retornarInt(removerTags(ler.readLine()));
+
+            ler.close();
+        }catch(FileNotFoundException e){
+            System.out.println("Erro em abrir file " + nomeArq);
+        }catch(IOException e){
+            System.out.println("Erro em ler file" + nomeArq);
+        }
+    }
+
+    private String procurarNome(String Arqu){
+        String nome = "";
+        for(int i = 0;i < Arqu.length();i++){
+            if(Arqu.charAt(i) == '_')
+                nome += ' ';
+            else
+                nome += Arqu.charAt(i);
+        }
+        return nome.substring(0, nome.length()-5);
+    }
+    private String removerTags(String in){
+        String resp = "";
+        for(int i = 0;i < in.length();i++){
+            if(in.charAt(i) == '<'){
+                i++;
+                while(in.charAt(i) != '>')
+                    i++;
+            }else if(in.charAt(i) == '&'){
+                i++;
+                while(in.charAt(i) != ';')
+                    i++;
+            }else{
+                resp += in.charAt(i);
+            }
+        }
+        return resp;
+    }
+    private int retornarInt(String in){
+        String resp = "";
+        for(int i = 0;i < in.length();i++){
+            if(in.charAt(i) >= '0' && in.charAt(i) <= '9'){
+                resp += in.charAt(i);
+            }else{
+                i = in.length();
+            }
+        }
+        return Integer.parseInt(resp);
+    }
+
+    //------------------------------------------------------------------------------
+    //Imprimir
+    public void imprimir(){
+        MyIO.print(nome + " " + formato + " " + duracao + " " + paisOrigem + " " + idiomaOrigem + " " + emissoraTV + " " + trasmissaoOriginal + " " + numTemp + " " + numEp + "\n");
+    }
+    //------------------------------------------------------------------------------
+}
+
+class Array{
+    private Serie series[];
+    private int comp;
+    private int mov;
+
+    //------------------------------------------------------------------------------
+    //Construtor
+    //------------------------------------------------------------------------------
+    public Array(){
+        this(61);
+    }
+    public Array(int tamanho){
+        series = new Serie[tamanho];
+        comp = mov = 0;
+    }
+    //------------------------------------------------------------------------------
+    //Gets
+    //------------------------------------------------------------------------------
+    public int getComp(){ return this.comp; }
+    public int getMov(){ return this.mov; }
+    //------------------------------------------------------------------------------
+    //Preencher
+    //------------------------------------------------------------------------------
+    public void preencher(){
+        int cont = 0;
+        String nomeArq = MyIO.readLine();
+        while(!(nomeArq.contains("FIM"))){
+            series[cont] = new Serie();
+            series[cont].ler(nomeArq);
+            cont++;
+            nomeArq = MyIO.readLine();
+        }
+    }
+    //------------------------------------------------------------------------------
+    //Sort
+    //------------------------------------------------------------------------------
+    public void sort(){
+        sort(0, (series.length - 1));
+    }
+    private void sort(int esq, int dir){
+        int i = esq;
+        int j = dir;
+        int pivo = (dir + esq) / 2;
+        while(i <= j){
+            while(iMenorPivo(i, pivo, false, 0)){ i++; }
+            while(jMaiorPivo(j, pivo, false, 0)){ j--; }
+            if(i <= j){
+                swap(i, j);
+                i++;
+                j--;
+            }
+        }
+        if(esq < i){ sort(esq, j); }
+        if(i < dir){ sort(i, dir); }
+    }
+    //------------------------------------------------------------------------------
+    //swap, getMaior, iMenorPivo, jMaiorPivo
+    //------------------------------------------------------------------------------
+    private void swap(int i, int j){
+        Serie tmp = series[i];
+        series[i] = series[j];
+        series[j] = tmp;
+        mov += 2;
+    }
+    private int getMaior(){
+        int posMaior = 0;
+        for(int i = 1;i < series.length;i++){
+            if(series[posMaior].getNumTemp() < series[i].getNumTemp()){
+                posMaior = i;
+            }
+        }
+        return posMaior;
+    }
+    private boolean iMenorPivo(int i, int pivo, boolean ehMenor, int cont){
+       if(series[i].getPaisOrigem().charAt(cont) == series[pivo].getPaisOrigem().charAt(cont)){
+            comp++;
+            cont++;    
+            iMenorPivo(i, pivo, ehMenor, cont);
+       }else if(series[i].getPaisOrigem().charAt(cont) < series[pivo].getPaisOrigem().charAt(cont)){
+           ehMenor = true;
+           comp++;
+       }
+
+        return ehMenor;
+    }
+    private boolean jMaiorPivo(int j, int pivo, boolean ehMenor, int cont){
+        if(series[j].getPaisOrigem().charAt(cont) == series[pivo].getPaisOrigem().charAt(cont)){
+             comp++;
+             cont++;    
+             iMenorPivo(j, pivo, ehMenor, cont);
+        }else if(series[j].getPaisOrigem().charAt(cont) > series[pivo].getPaisOrigem().charAt(cont)){
+            ehMenor = true;
+            comp++;
+        }
+ 
+         return ehMenor;
+     }
+    //------------------------------------------------------------------------------
+    //Mostrar
+    //------------------------------------------------------------------------------
+    public void mostrar(){
+        for(int i = 0;i < series.length;i++){
+            series[i].imprimir();
+        }
+    }
+    //------------------------------------------------------------------------------
+}
+
+
+class QuickSort{
+    public static void main(String[] args){
+        long inicio = System.currentTimeMillis();
+        Array array = new Array();
+        array.preencher();
+        array.sort();
+        array.mostrar();
+        long fim = System.currentTimeMillis();
+        long tempo = fim - inicio;
+        int comp = array.getComp();
+        int mov = array.getMov();
+        Arq arquivo = new Arq();
+        arquivo.openWriteClose("656016_quicksort.txt", "656016\t" + comp + "\t" + mov + "\t" + tempo);
+    }
+}
